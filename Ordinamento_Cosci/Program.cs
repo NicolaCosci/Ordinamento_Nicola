40aeeae163f560c6ed574029bccee59f2a519976
Nicola Cosci <nicola2002.cosci@gmail.com>
2019-11-26 17:43:47 +0100

    Aggiungere i file di progetto.

---
+++ Ordinamento_Cosci/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordinamento_Cosci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X = 0;
+            double Y = 0;
+            double Z = 0;
+            double K = 0;
+            Console.WriteLine("Dai un valore a X Y e Z :");
+            X = Convert.ToDouble(Console.ReadLine());
+            Y = Convert.ToDouble(Console.ReadLine());
+            Z = Convert.ToDouble(Console.ReadLine());
+            var LeggiX = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine(X);
+
+            });
+
+            var LeggiY = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine(Y);
+
+            });
+
+
+            var LeggiZ = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine(Z);
+
+            });
+
+
+            var kmassimo1 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(500);
+                if (X > Y)
+                {
+                    K = X;
+                }
+                else
+                {
+                    K = Y;
+                }
+
+            });
+            var kmassimo2 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                if (K < Z)
+                {
+                    K = Z;
+                }
+            });
+
+
+
+            var Stampa = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("il numero max è");
+                Console.WriteLine(K);
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ordinamento_Nicola/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordinamento_Nicola
+{ 
+    class Program 
+    {
+        static void Main(string[] args)  
+        {
+            int risuno = 0, riscinque = 0, risdue = 0, ristre = 0, risquattro = 0; 
+             
+            var PrimaOperazione = Task.Factory.StartNew(() =>
+            {
+                risuno = 2 * 6;
+
+            });
+
+            var SecondaOperazione = Task.Factory.StartNew(() =>
+            {
+                risdue = 1 + 4;
+
+
+            });
+
+            var TerzaOperazione = Task.Factory.StartNew(() =>
+            {
+                ristre = 5 - 2;
+
+            });
+
+            var QuartaOperazione = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(500);
+                risquattro = ristre * risdue;
+
+            });
+
+            var QuintaOperazione = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                riscinque = risuno + risquattro;
+                Console.WriteLine(riscinque);
+            });
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
